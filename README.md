70a6b6f3dcc59561f8dd3097456602d38f71cdab
Renato Alencar <renatoalencar.73@gmail.com>
2021-01-19 22:20:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Clojure Empregos Brasil
+
+Vagas de empresas que ativamente contratam pessoas desenvolvedoras Clojure no Brasil. Quer anunciar uma vaga? Abra uma [issue](issues).
+
+## Vagas
+
+
+|                                                                           Vaga |    Empresa |         Local | Remoto? |                                                                                                            Onde aplicar |
+|--------------------------------------------------------------------------------|------------|---------------|---------|-------------------------------------------------------------------------------------------------------------------------|
+|                                                              Software Engineer |     Nubank |     São Paulo |     Não |                                                                 https://boards.greenhouse.io/nubank/nubank/jobs/2569175 |
+|                                                                   Tech Manager |     Nubank |     São Paulo |     Não |                                                                 https://boards.greenhouse.io/nubank/nubank/jobs/2185260 |
+|                                                            Tech Lead (Clojure) |      PayGo |     São Paulo |     Não |                                                      https://paygo.gupy.io//jobs/462412?jobBoardSource=gupy_public_page |
+|                          Remote Senior Back-End / Fullstack Engineer - Clojure |    Embraer |               |     Sim |                                                    https://embraer.gupy.io//jobs/536712?jobBoardSource=gupy_public_page |
+|             Engenheira de Software (vaga focada em mulheres) - Trabalho Remoto | Pipo Saúde | São Paulo, BR |         |             https://pipo-saude.breezy.hr//p/2508984cb6c6-engenheira-de-software-vaga-focada-em-mulheres-trabalho-remoto |
+|                                                  Pessoa Engenheira de Software | Pipo Saúde | São Paulo, BR |         |                                              https://pipo-saude.breezy.hr//p/4339d952332d-pessoa-engenheira-de-software |
+|                  Pessoa Engenheira de Software (vaga focada em pessoas negras) | Pipo Saúde | São Paulo, BR |         |                https://pipo-saude.breezy.hr//p/1a9152c5e824-pessoa-engenheira-de-software-vaga-focada-em-pessoas-negras |
+| Pessoa Engenheira de Software (vaga focada em pessoas trans) - Trabalho Remoto | Pipo Saúde | São Paulo, BR |         | https://pipo-saude.breezy.hr//p/647061946892-pessoa-engenheira-de-software-vaga-focada-em-pessoas-trans-trabalho-remoto |
+
+
---
+++ src/clojure_empregos_brasil/scraping.clj
@@ -0,0 +1,120 @@
+(ns clojure-empregos-brasil.scraping
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.pprint :as pprint]
+            [clojure.set :as set]
+            [selmer.parser :as selmer]
+            [clojure.string :as string]))
+
+(defn attr [x]
+  (fn [node]
+    (get (:attrs node) x)))
+
+(defn scrap
+  [html path mapping]
+  (for [opening (html/select html path)]
+    (into {}
+          (for [[key value-path] mapping]
+            [key
+             (cond
+               (vector? value-path) (first (html/select opening value-path))
+               (fn? value-path) (value-path opening)
+               :else value-path)]))))
+
+(def nubank
+  {:name      "Nubank"
+   :page      "https://boards.greenhouse.io/nubank"
+   :path      [:div.opening]
+   :scrap     {:title      [:a html/text-node]
+               :url        #(-> % (html/select [:a]) first :attrs :href)
+               :location   [:.location html/text-node]
+               :department (attr :department_id)
+               :office_id  (attr :office_id)
+               :remote     false}
+   :engineer? (comp (partial = "60350") :department)
+   :brazil?   (comp (partial = "58102") :office_id)
+   :clojure?  boolean
+   :enrich    identity})
+
+(def gupy
+  {:title      [:.title html/text-node]
+   :url        #(-> % (html/select [:a.job-list__item]) first :attrs :href)
+   :location   (attr :data-workplace)
+   :department (attr :data-department)
+   :remote     (comp (partial = "true") (attr :data-remote))
+   :type       (attr :data-type)})
+
+(def paygo
+  {:name      "PayGo"
+   :page      "https://paygo.gupy.io/"
+   :path      [:.job-list :tr]
+   :scrap     gupy
+   :engineer? #(= (:department %) "Tecnologia")
+   :brazil?   boolean
+   :clojure?  #(string/includes?
+                 (-> % :title string/lower-case)
+                 "clojure")})
+
+(def embraer
+  {:name      "Embraer"
+   :page      "https://embraer.gupy.io/"
+   :path      [:.job-list :tr]
+   :scrap     gupy
+   :engineer? #(= (:department %) "Inovação")
+   :brazil?   :remote
+   :clojure?  #(string/includes?
+                 (-> % :title string/lower-case)
+                 "clojure")})
+
+(def pipo-saude
+  {:name      "Pipo Saúde"
+   :page      "https://pipo-saude.breezy.hr/"
+   :path      [:li.position]
+   :scrap     {:title      [:h2 html/text-node]
+               :url        #(-> % (html/select [:a]) first :attrs :href)
+               :location   [:ul.meta :li.location html/text-node]
+               :department [:ul.meta :li.department html/text-node]
+               :type       [:ul.meta :li.type html/text-node]}
+   :engineer? #(= (:department %) "Engenharia")
+   :brazil?   boolean
+   :clojure?  boolean})
+
+(defn scrap-all [& companies]
+  (flatten
+    (for [{:keys [engineer? brazil? clojure? enrich name page] :as company} companies]
+      (let [html (html/html-resource (java.net.URL. page))
+            positions (scrap html
+                             (:path company)
+                             (:scrap company))]
+        (->> positions
+             (filter #(and (brazil? %) (clojure? %) (engineer? %)))
+             (map #(assoc % :name name
+                            :url (str page (:url %)))))))))
+
+(defn markdown-table
+  [x]
+  (string/replace (with-out-str
+                    (pprint/print-table x))
+                  #"\+" "|"))
+
+(defn translate-values
+  [x]
+  (-> x
+      (update :remote {true "Sim" false "Não"})
+      (update :title #(string/replace % #"\|" "-"))))
+
+(defn -main [& args]
+  (let [positions (scrap-all nubank paygo embraer pipo-saude)]
+    (spit "scraped-jobs.edn" (pr-str positions))
+
+    (->> {:vagas (markdown-table
+                   (map #(-> %
+                             (select-keys [:title :name :location :remote :url])
+                             translate-values
+                             (set/rename-keys {:title    "Vaga"
+                                               :name     "Empresa"
+                                               :location "Local"
+                                               :remote   "Remoto?"
+                                               :url      "Onde aplicar"}))
+                        positions))}
+         (selmer/render (slurp "README.md.template"))
+         (spit "README.md"))))
